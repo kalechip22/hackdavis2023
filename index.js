5fd48c4fadb3eb9c09e90c74db577c542ea80f32
kalechip <kayla.hom22@gmail.com>
2023-05-20 15:46:55 -0700

    server added

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb+srv://hackdavis:moomoo@mimo.pzmvxek.mongodb.net/?retryWrites=true&w=majority")
+
+app.get("/", function(req, res) {
+    res.send("express is working")
+})
+// app.post
+
+app.listen(3000, function() {
+    console.log("server is running on 3000")
+})
