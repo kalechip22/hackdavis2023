11cefbe0db2c84cda6aaa09a99ab25993d0ab5cf
kalechip <kayla.hom22@gmail.com>
2023-05-21 05:11:57 -0700

    finished new entry

--- public/portal.js
+++ public/portal.js
@@ -5,13 +5,14 @@
 // })
 //    .then(response => response.text())
 //    .then(text => console.log(text))
+const buttons = document.getElementsByTagName("button");
+
 
 fetch('/all-entries', {
     headers: {
        'Accept': 'application/json'
     }
  }) .then(function (response) {
-    console.log("here");
     return response.json();
  }) .then(function(obj) {
    let i = 0;
@@ -19,7 +20,7 @@ fetch('/all-entries', {
         i++;
         let card = document.createElement("button");
         card.className = "card_entries";
-        card.setAttribute("id", "button"+i);
+        card.setAttribute("id", i);
 
         let title = document.createElement("div");
         title.className = "title_entry";
@@ -30,26 +31,73 @@ fetch('/all-entries', {
         let date = document.createElement("div");
         date.className = "date_entry"
 
-        let content = document.createElement("div");
-        let contentText = document.createTextNode(res.content);
-        
-
         let wholeDate = res.date;
-        console.log(wholeDate);
         let year = wholeDate.substring(0, 4);
         let month = wholeDate.substring(5, 7);
         let day = wholeDate.substring(8, 10);
         let dateText = document.createTextNode(month + "\\" + day + "\\" + year);
         date.appendChild(dateText);
-        card.appendChild(date);;
+        card.appendChild(date);
 
-        // let date = document.createTextNode('date:' + res.date);
-        // card.appendChild(date);
+        let shadowBox = document.createElement("div");
+        shadowBox.className = "box hide"
+        shadowBox.setAttribute("id", "box"+i);
+        let content = document.createElement("div");
+        let contentText = document.createTextNode(res.content);
+        content.className = "content hide"
+        content.appendChild(contentText);
+        content.setAttribute("id", "text"+i);
+        card.appendChild(content);
+        card.appendChild(shadowBox);
 
         let container = document.querySelector("#entries");
         container.appendChild(card);
+        
+      //   let container2 = document.getElementsByClassName("main");
+      //   container2.appendChild(shadowBox);
     });
+    let btns = document.querySelectorAll('button');
+
+    for (i of btns) {
+      i.addEventListener('click', function() {
+        let find = "#text"+this.id
+        console.log(find);
+
+        let overlay = document.querySelector(find);
+        overlay.className = "content show";
+
+        let find2 = "#box"+this.id
+
+        let overlay2 = document.querySelector(find2);
+        overlay2.className = "box show";
+      });
+    }
+    
     console.log(obj);
  }) .catch(function (error) {
     console.error(error);
  });
+
+
+//  const wrapper = document.getElementById('entries');
+
+//  wrapper.addEventListener('click', (event) => {
+//     const isButton = event.target.nodeName === 'BUTTON';
+//     if (!isButton) {
+//     return;
+//     }
+//     console.log(event.target.id);
+//     let overlay = document.querySelector("text"+event.target.id);
+//     overlay.className = "content show";
+
+
+
+
+
+
+ 
+//     console.dir();
+//  })
+
+
+
